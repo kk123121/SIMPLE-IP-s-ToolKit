db3b1763a70b9b3cc1abe26e35872a82f4924253
Simple IP <kaidenjclaflin@gmail.com>
2024-09-01 13:36:59 -0700

    Update For1st10downloader.bat

--- For1st10downloader.bat
+++ For1st10downloader.bat
@@ -28,6 +28,10 @@ echo    3.IPCreator
 echo    4.Discord.py installer
 echo    5.python installer
 echo    6.RPG game :3 (i cant think of anything else......)
+echo    7.Hacking tools!
+echo    8.Progams to test/use
+echo    9.Free VPNS
+echo    10.Websites that attack
 echo    ----------------------------------------------------
 
 set /p menu="Please type the number for what you want! "
@@ -55,6 +59,23 @@ if %menu%==5 (
 if %menu%==6 (
     goto 6
 )
+
+if %menu%==7 (
+    goto 7
+)
+
+if %menu%==8 (
+    goto 8
+)
+
+if %menu%==9 (
+    goto 9
+)
+
+if %menu%==10 (
+    goto 10
+)
+
 
 
 :1
@@ -319,3 +340,93 @@ cls
 echo Game Over! You have been defeated.
 pause
 goto menu
+
+
+
+:7
+cls
+title Hacking Devices
+color d
+echo    Device     and    Where to get
+echo   _____________  ________________
+echo 1.Flipper Zero-flipper Zero
+echo 2.hackrf (best one to get is portable)-almost anywhere that sells devices
+echo 3.wifi-nugget-retia.io
+echo 4.m5stickplus 2 or m5 for short tho, m5stack
+echo 5.rasberry PI (running kali linux)-rassberry pi official
+echo 6.bad usb-bad usb
+echo 7.tv-b-gone-amazon
+echo 8.deauthar watch-ali express
+echo 9.Back
+
+set /p hackingdeviceinput=
+if "%hackingdeviceinput%" EQU "9" cls goto m
+cls 
+goto m
+
+
+
+:8
+cls
+title Programs
+color 1
+echo Programs:
+echo 1.Virtual box
+echo 2.angryip
+echo 3.Back
+ set /p programsinput=
+if /I "%programsinput%" EQU "1" start https://www.virtualbox.org/
+if /I "%programsinput%" EQU "2" start https://angryip.org/
+if /I "%programsinput%" EQU "3" cls goto m
+cls
+goto m
+
+:9
+cls
+title Free VPNS
+color A
+echo Free VPNS:
+echo 1.proton vpn
+echo 2.urban vpn
+echo 3.planet vpn
+echo 4.gratis vpn (THIS VPN IS FOR CHROME ONLY)
+echo 5.atlas vpn
+echo 6.touch vpn
+echo 7.itop vpn
+echo 8.Back
+ set /p vpninput=
+if /I "%vpninput%" EQU "1" start https://account.protonvpn.com/signup?plan=free&curremcy=EUR&ref=upsell
+if /I "%vpninput%" EQU "2" start https://www.urban-vpn.com/
+if /I "%vpninput%" EQU "3" start https://freevpnplanet.com/
+if /I "%vpninput%" EQU "4" start https://chromewebstore.google.com/detail/gratis-vpn-voor-chrome-fr/jcbiifklmgnkppbelchllpdbnibihel?hl=nl
+if /I "%vpninput%" EQU "5" start https://atlasvpn.com/free-vpn
+if /I "%vpninput%" EQU "6" start https://www.touchvpn.net/
+if /I "%vpninput%" EQU "7" start https://www.itopvpn.com/free-vpn
+if /I "%vpninput%" EQU "8" cls goto m
+cls
+goto m
+
+:10
+cls
+color 4
+title Websites that attack
+echo Websites:
+echo 1.jailbroken gpts
+echo ddos:
+echo 2.https://freestresser.so/
+echo 3.https://hardstresser.com/
+echo 4.https://stresser.net/
+echo 5.https://str3ssed.co/
+echo 6.https://projectdeltastress/com/
+echo 7.Back
+
+set /p webinput=
+if /I "%webinput%" EQU "1" start https://github.com/friuns2/BlackFriday-GPTs-Prompts/blob/main/2024-May-Update.md?plain=1
+if /I "%webinput%" EQU "2" start https://freestresser.so/
+if /I "%webinput%" EQU "3" start https://hardstresser.com/
+if /I "%webinput%" EQU "4" start https://stresser.net/
+if /I "%webinput%" EQU "5" start https://str3ssed.net/
+if /I "%webinput%" EQU "6" start https://projectdeltastress.com/
+if /I "%webinput%" EQU "7" cls goto m
+cls
+goto m
